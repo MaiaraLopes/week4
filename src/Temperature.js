c182018ef3e1f73e9b1ac584137ccfa346752a19
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-02-07 16:53:05 +0000

    Installed bootstrap

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Temperature.css";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 export default function Temperature() {
   return (
